a3c0e346a372a3f905e796d4dd5373aac8f5f96b
a22lauracv <a22lauracv@debian.a22lauracv>
2022-11-11 21:37:34 +0100

    Ejercicios realizados y añadidos

--- tareas/ejercicio1.java
+++ tareas/ejercicio1.java
@@ -1,11 +1,62 @@
+import java.util.Scanner;
+ 
+public class Ejercico1 {
+ 
+    public static void main(String[] args) {
+        String horoscopo = "";
+        System.out.println("Introduce tu año de nacimiento: ");
+        Scanner sc = new Scanner(System.in);
+        int anho = sc.nextInt();
+        int resto = anho % 12;
+        switch (resto) {
+            case 0: 
+                horoscopo = "Mono"; 
+                break;
+            case 1: 
+                horoscopo = "Gallo";
+                break;
+            case 2: 
+                horoscopo = "Perro";
+                break;
+            case 3: 
+                horoscopo = "Cerdo"; 
+                break;
+            case 4: 
+                horoscopo = "Rata"; 
+                break;
+            case 5: 
+                horoscopo = "Buey"; 
+                break;
+            case 6: 
+                horoscopo = "Tigre"; 
+                break;
+            case 7: 
+                horoscopo = "Conejo"; 
+                break;
+            case 8: 
+                horoscopo = "Dragon"; 
+                break;
+            case 9: 
+                horoscopo = "Serpiente"; 
+                break;
+            case 10: 
+                horoscopo = "Caballo"; 
+                break;
+            case 11: 
+                horoscopo = "Cabra"; 
+                break;
+        }
+ 
+        System.out.println("Has nacido en el año " + anho + ", te corresponde el signo " + horoscopo + " en el horóscopo chino.");
+    }   
+
+}
+
 /*
  * Signo del zodíaco
- * Crea que al introducir el año que naciste te devuelva
-tu signo del zodíaco chino.
-Para conocer el signo del horóscopo chino, debemos
-dividir el año de nacimiento entre 12. El resto, entre
-0 y 11, está asociado a un signo según la siguiente
-tabla:
+ * Crea que al introducir el año que naciste te devuelva tu signo del zodíaco chino.
+Para conocer el signo del horóscopo chino, debemos dividir el año de nacimiento entre 12. El resto, entre
+0 y 11, está asociado a un signo según la siguiente tabla:
 
 0 -Mono
 1- Gallo
--- tareas/ejercicio2.java
+++ tareas/ejercicio2.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+ 
+public class Ejercicio2 {
+ 
+    public static void main(String[] args) {
+        System.out.println("Introduce al menos 4 nombres de persona: ");
+        System.out.println("¿Cúantos nombres quieres añadir?");
+        Scanner sc = new Scanner(System.in);
+        int numNombres = sc.nextInt();
+        sc.nextLine();
+        if (numNombres < 4) {
+            System.out.println("El número de nombres introducido es incorrecto");
+        } else {
+            String resultado = "¡Hola "; 
+            for (int i = 0; i < numNombres; i++) {
+                String nombre = sc.nextLine();
+                resultado = resultado + nombre;
+                if (numNombres > (i + 1)) {
+                    resultado = resultado + ", ";
+                }
+            }
+            System.out.println(resultado + " !");      
+        }
+    }   
+
+}
+
 /*
  * 
-Haz un script que reciba por parámetro al menos 4 nombres de persona y devuelva un saludo separado por comas del tipo “¡Hola Ana, Andrés, Juan, Manuel !”
+Haz un script que reciba por parámetro al menos 4 nombres de persona y 
+devuelva un saludo separado por comas del tipo “¡Hola Ana, Andrés, Juan, Manuel !”
 
  */
